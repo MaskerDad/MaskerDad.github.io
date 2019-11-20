8d5d4acb370494bcdb73c6cc72337aad8f3508b1
Albert_yzp <1261418485@qq.com>
2019-11-20 17:39:25 +0800

    post

--- DeployGiteePage.py
+++ DeployGiteePage.py
@@ -12,8 +12,7 @@ driver.get(url)
 
 #下面就是登陆自己的账号。登陆账号之后才能部署
 #找到右上角登陆的按钮并点击
-driver.find_element_by_xpath('//*[@id="git-nav-user-bar"]/a[2]').click()
-
+driver.find_element_by_xpath('//*[@id="git-nav-user-bar"]/a[1]').click()
 
 #输入用户名
 driver.find_element_by_xpath('//*[@id="user_login"]').send_keys("15202962739")
